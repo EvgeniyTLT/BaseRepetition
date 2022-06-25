c7c6cc04767f06c37ef1dbd29a5277eb6983d3aa
EvgeniyTLT <Toy444@ya.ru>
2022-06-25 22:41:57 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -3,33 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int[] array = new int[1000];
+        int[] array = new int[10];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 10000);
+            array[i] = (int) (Math.random() * 100);
         }
-        int max = 0;
-        for (int u : array) {
-            if (u > max) {
-                max = u;
+        int sumMax=0;
+        int sumArray=0;
+        int index = 0;
+        for (int i = 0; i < array.length-2; i++) {
+            sumArray = array[i]+array[i+1]+array[i+2];
+            if (sumMax < sumArray){
+                sumMax=sumArray;
+                index=i;
             }
         }
-        System.out.println("max: " + max);
-        int min = 10000;
-        for (int u : array) {
-            if (u < min) {
-                min = u;
-            }
+        for (int e: array
+             ) {
+            System.out.print(e+ " ");
         }
-        System.out.println("min: " + min);
-        int counter5 = 0;
-        int sum5 = 0;
-        for (int u : array) {
-            if (u % 5 == 0) {
-                counter5++;
-                sum5 += u;
-            }
-        }
-        System.out.println("counter5: " + counter5+ " sum5: " + sum5);
+        System.out.println("\nmaxSum: "+sumMax+" index: " + index);
 
     }
 }
