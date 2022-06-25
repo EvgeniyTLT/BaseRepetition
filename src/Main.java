f519d04ab320b9f8328125a760d913190583eb30
EvgeniyTLT <Toy444@ya.ru>
2022-06-25 22:27:07 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int x = scanner.nextInt();
-        int[] array = new int[x];
-        for (int i = 0; i < x; i++) {
-            array[i] = (int) (Math.random() * 10);
-        }
 
+        int[] array = new int[1000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 10000);
+        }
+        int max = 0;
         for (int u : array) {
-            System.out.println(u);
-        }
+            if (u > max) {
+                max=u;
+            }
 
+        }
+        System.out.println(max);
     }
 
 }
