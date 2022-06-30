701d4c956cd8dde51261cef0dad7660b8e79749c
EvgeniyTLT <Toy444@ya.ru>
2022-06-30 23:26:56 +0300

    add

---
+++ src/lesson13/MainTask1.java
@@ -0,0 +1,14 @@
+package lesson13;
+
+public class MainTask1 {
+    public static void main(String[] args) {
+        User user1 = new User("AAA");
+        User user2 = new User("BBB ");
+        user1.sendMessage(user2, "Hello BBB");
+        user1.sendMessage(user2, "How are you?");
+        user2.sendMessage(user1, "Bay");
+
+        MessageDatabase.showDialog(user1, user2);
+
+    }
+}
