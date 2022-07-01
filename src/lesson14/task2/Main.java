68e1229a81994fd0718dea2b37090ad3cb713031
EvgeniyTLT <Toy444@ya.ru>
2022-07-02 01:38:41 +0300

    add

---
+++ src/lesson14/task2/Main.java
@@ -0,0 +1,36 @@
+package lesson14.task2;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(parseFileToStringList());
+
+    }
+
+    public static List<String> parseFileToStringList() {
+        File file = new File("people.txt");
+        List<String> people = new ArrayList<>();
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] person = line.split(" ");
+                if (Integer.parseInt(person[1]) <= 0) throw new IllegalArgumentException();
+                people.add(line);
+            }
+            return people;
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный входной файл");
+        }
+        return null;
+    }
+}
