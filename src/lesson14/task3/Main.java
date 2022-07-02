ebf4963ff6013121d29b089b5d1c025b0440f38a
EvgeniyTLT <Toy444@ya.ru>
2022-07-02 22:59:03 +0300

    add

--- src/lesson14/task3/Main.java
+++ src/lesson14/task3/Main.java
@@ -1,38 +1,33 @@
-package lesson14.task2;
+package lesson14.task3;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-
-
-
     public static void main(String[] args) {
-        System.out.println(parseFileToStringList());
-
+        System.out.println(parseFileToObjList());
     }
 
-    public static List<String> parseFileToStringList() {
+
+    public static List<People> parseFileToObjList() {
         File file = new File("people.txt");
-        List<String> people = new ArrayList<>();
         try {
             Scanner scanner = new Scanner(file);
+            List<People> peopleList = new ArrayList<>();
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] person = line.split(" ");
-                if (Integer.parseInt(person[1]) <= 0) throw new IllegalArgumentException();
-                people.add(line);
+                if (Integer.parseInt(person[1]) < 0) throw new IllegalArgumentException();
+                peopleList.add(new People(person[0], Integer.parseInt(person[1])));
             }
-            return people;
+            return peopleList;
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден");
         } catch (IllegalArgumentException e) {
-            System.out.println("Некорректный входной файл");
+            System.out.println("Отрицательный возраст");
         }
         return null;
     }
---
+++ src/lesson14/task3/People.java
@@ -0,0 +1,35 @@
+package lesson14.task3;
+
+public class People {
+    private String name;
+    private int year;
+
+    public People(String name, int year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", year=" + year +
+                '}';
+    }
+}
