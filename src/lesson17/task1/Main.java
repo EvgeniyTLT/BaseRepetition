d91af83df9dfaf46efe1bdfa24c40383c81eec4b
EvgeniyTLT <Toy444@ya.ru>
2022-07-04 23:57:09 +0300

    add

--- src/lesson17/task1/Main.java
+++ src/lesson17/task1/Main.java
@@ -1,4 +1,18 @@
 package lesson17.task1;
 
 public class Main {
+    public static void main(String[] args) {
+        ChessPiece[] chessPieces = new ChessPiece[8];
+        ChessPiece chessPiece1 = ChessPiece.PAWN_WRITE;
+        ChessPiece chessPiece2 = ChessPiece.ROOK_BLACK;
+        for (int i = 0; i < 8; i++) {
+            if (i < 4) chessPieces[i] = chessPiece1;
+            else chessPieces[i] = chessPiece2;
+        }
+        for (ChessPiece u : chessPieces
+        ) {
+            System.out.print(u.getName());
+        }
+    }
 }
+
