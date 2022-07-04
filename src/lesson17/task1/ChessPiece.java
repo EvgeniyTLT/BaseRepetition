477bbc9c0f83817f039c24e3bc61ab00313f6ff2
EvgeniyTLT <Toy444@ya.ru>
2022-07-05 00:04:29 +0300

    add

--- src/lesson17/task1/ChessPiece.java
+++ src/lesson17/task1/ChessPiece.java
@@ -3,36 +3,37 @@ package lesson17.task1;
 public enum ChessPiece {
     KING_WHITE(100, "♔"), KING_BLACK(100, "♚"), QUEEN_WHITE(9, "♕"), QUEEN_BLACK(9, "♛"),
     ROOK_WHITE(5, "♖"), ROOK_BLACK(5, "♜"), BISHOP_WRITE(3.5, "♗"), BISHOP_BLACK(3.5, "♝"),
-    KNIGHT_WRITE(3, "♘"), KNIGHT_BLACK(3, "♞"), PAWN_WRITE(1, "♙"), PAWN_BLACK(1, "♟");
-    private double level;
-    private String name;
+    KNIGHT_WRITE(3, "♘"), KNIGHT_BLACK(3, "♞"), PAWN_WRITE(1, "♙"), PAWN_BLACK(1, "♟"),
+    EMPTY(-1, "_");
+    private double value;
+    private String representation;
 
-    ChessPiece(double level, String name) {
-        this.level = level;
-        this.name = name;
+    ChessPiece(double level, String representation) {
+        this.value = level;
+        this.representation = representation;
     }
 
-    public double getLevel() {
-        return level;
+    public double getValue() {
+        return value;
     }
 
-    public void setLevel(int level) {
-        this.level = level;
+    public void setValue(int value) {
+        this.value = value;
     }
 
     public String getName() {
-        return name;
+        return representation;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.representation = name;
     }
 
     @Override
     public String toString() {
         return "ChessPiece{" +
-                "level=" + level +
-                ", name='" + name + '\'' +
+                "value=" + value +
+                ", representation='" + representation + '\'' +
                 '}';
     }
 }
---
+++ src/lesson17/task2/ChessBoard.java
@@ -0,0 +1,11 @@
+package lesson17.task2;
+
+import lesson17.task1.ChessPiece;
+
+public class ChessBoard {
+    private ChessPiece[][] chessBoard;
+
+    public ChessBoard(ChessPiece[][] chessBoard) {
+        this.chessBoard = chessBoard;
+    }
+}
