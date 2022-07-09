55fa0fc6d4939f74c576528ffd519c339cd32388
EvgeniyTLT <Toy444@ya.ru>
2022-07-09 23:27:00 +0300

    add

--- src/lesson18/Main.java
+++ src/lesson18/Main.java
@@ -2,14 +2,15 @@ package lesson18;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<String > animals = new ArrayList<>();
 
-        animals.add("asfsdfad");
-        animals.add("shfjhsdj");
+        animals.add("asfsdfadfsdfgd");
+        animals.add("s");
         animals.add("thnrng");
         animals.add("nbdfbnd");
         animals.add("vsiooidgs");
@@ -17,7 +18,16 @@ public class Main {
         animals.add("nmf");
         animals.add("шаппв");
 
-        Collections.sort(animals);
+        Collections.sort(animals, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.length()>o2.length()){
+                    return 1;
+                } else if (o1.length()<o2.length()){
+                    return -1;
+                } else return 0;
+            }
+        });
         System.out.println(animals);
 
 
