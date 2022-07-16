d780aed561841572bff9f92c4c567534d70d5a97
EvgeniyTLT <Toy444@ya.ru>
2022-07-16 16:38:06 +0300

    add

---
+++ src/lesson22/Main.java
@@ -0,0 +1,58 @@
+package lesson22;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Main {
+    private static BlockingQueue<Integer> queue = new ArrayBlockingQueue<>(10);
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    produse();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    consumer();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread1.start();
+        thread2.start();
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void produse() throws InterruptedException {
+        Random random = new Random();
+        while (true) {
+            queue.put(random.nextInt(100));
+        }
+    }
+
+    private static void consumer() throws InterruptedException {
+        Random random = new Random();
+        while (true) {
+            Thread.sleep(100);
+            if (random.nextInt(10)==5) {
+                System.out.println(queue.take());
+                System.out.println("Size: " + queue.size());
+            }
+        }
+    }
+}
