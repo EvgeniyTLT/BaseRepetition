b48cb2d3749ca2cb90b827e1eddb5a26491986ba
EvgeniyTLT <Toy444@ya.ru>
2022-07-20 23:00:25 +0300

    add

--- src/lesson28/Main.java
+++ src/lesson28/Main.java
@@ -1,6 +1,8 @@
 package lesson28;
 
 import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
@@ -31,18 +33,34 @@ class Runner {
 
     private Account account1 = new Account();
     private Account account2 = new Account();
+    private Lock lock1 = new ReentrantLock();
+    private Lock lock2 = new ReentrantLock();
 
     public void firstThread() {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            Account.transfer(account1, account2, random.nextInt(100));
+            lock1.lock();
+            lock2.lock();
+            try {
+                Account.transfer(account1, account2, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
     }
 
     public void secondThread() {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            Account.transfer(account2, account1, random.nextInt(100));
+            lock1.lock();
+            lock2.lock();
+            try {
+                Account.transfer(account2, account1, random.nextInt(100));
+            } finally {
+                lock1.unlock();
+                lock2.unlock();
+            }
         }
     }
 
