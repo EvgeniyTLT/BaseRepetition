4cbc73ca57df544eb51578028006028e7f391366
EvgeniyTLT <Toy444@ya.ru>
2022-07-20 23:18:34 +0300

    add

--- src/lesson28/Main.java
+++ src/lesson28/Main.java
@@ -36,11 +36,36 @@ class Runner {
     private Lock lock1 = new ReentrantLock();
     private Lock lock2 = new ReentrantLock();
 
+    private void takeLocks(Lock lock1, Lock lock2) {
+        boolean firstLockTaken = false;
+        boolean secondLockTaken = false;
+        while (true) {
+            try {
+                firstLockTaken = lock1.tryLock();
+                secondLockTaken = lock2.tryLock();
+            } finally {
+                if (firstLockTaken && secondLockTaken) {
+                    return;
+                }
+                if (firstLockTaken) {
+                    lock1.unlock();
+                }
+                if (secondLockTaken) {
+                    lock2.unlock();
+                }
+            }
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public void firstThread() {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            lock1.lock();
-            lock2.lock();
+            takeLocks(lock1, lock2);
             try {
                 Account.transfer(account1, account2, random.nextInt(100));
             } finally {
@@ -53,8 +78,7 @@ class Runner {
     public void secondThread() {
         Random random = new Random();
         for (int i = 0; i < 10000; i++) {
-            lock1.lock();
-            lock2.lock();
+            takeLocks(lock2, lock1);
             try {
                 Account.transfer(account2, account1, random.nextInt(100));
             } finally {
