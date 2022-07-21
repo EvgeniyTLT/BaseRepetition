982244857b911d61c7ad71368c904a92bc83447f
EvgeniyTLT <Toy444@ya.ru>
2022-07-21 23:36:23 +0300

    add

--- src/lesson29/Main.java
+++ src/lesson29/Main.java
@@ -1,6 +1,8 @@
 package lesson29;
 
+import javax.sound.midi.Soundbank;
 import java.util.Random;
+import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,20 +11,23 @@ public class Main {
             public void run() {
                 Random random = new Random();
                 for (int i = 0; i < 1_000_000_000; i++) {
-                    Thread.currentThread().isInterrupted();
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println("Thread stop");
+                        break;
+                    }
                     double x = Math.sin(random.nextDouble());
                     System.out.println(i + " : " + x);
                 }
             }
         });
-
         System.out.println("Start");
-
         thread.start();
-        thread.stop();
+
         try {
-            thread.join();
+            Thread.sleep(4000);
             thread.interrupt();
+            thread.join();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
