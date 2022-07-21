7ecbd339a1ebadee7bd1d34bf6f933ab6cb40e1a
EvgeniyTLT <Toy444@ya.ru>
2022-07-21 23:32:44 +0300

    add

--- src/lesson29/Main.java
+++ src/lesson29/Main.java
@@ -9,6 +9,7 @@ public class Main {
             public void run() {
                 Random random = new Random();
                 for (int i = 0; i < 1_000_000_000; i++) {
+                    Thread.currentThread().isInterrupted();
                     double x = Math.sin(random.nextDouble());
                     System.out.println(i + " : " + x);
                 }
@@ -16,12 +17,16 @@ public class Main {
         });
 
         System.out.println("Start");
+
         thread.start();
+        thread.stop();
         try {
             thread.join();
+            thread.interrupt();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        thread.stop();
         System.out.println("Finish");
     }
 
